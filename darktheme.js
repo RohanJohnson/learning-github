193f38fbdf20ae50dfd3379b767bc19b4816fe6e
RohanJohnson <21000165@students.gateshead.ac.uk>
2022-12-13 13:20:24 +0000

    Formatted code

--- darktheme.js
+++ darktheme.js
@@ -7,16 +7,16 @@ const darkThemeStylesheet = document.querySelector('#dark-theme');
 
 // Function to switch between the light and dark theme stylesheets
 function switchTheme() {
-  // If the dark theme stylesheet is applied, remove it and apply the light theme stylesheet
-  if (darkThemeStylesheet.disabled) {
-    darkThemeStylesheet.disabled = false;
-    lightThemeStylesheet.disabled = true;
-  }
-  // Otherwise, apply the dark theme stylesheet
-  else {
-    darkThemeStylesheet.disabled = true;
-    lightThemeStylesheet.disabled = false;
-  }
+    // If the dark theme stylesheet is applied, remove it and apply the light theme stylesheet
+    if (darkThemeStylesheet.disabled) {
+        darkThemeStylesheet.disabled = false;
+        lightThemeStylesheet.disabled = true;
+    }
+    // Otherwise, apply the dark theme stylesheet
+    else {
+        darkThemeStylesheet.disabled = true;
+        lightThemeStylesheet.disabled = false;
+    }
 }
 
 // Add an event listener to the toggle button that calls the switchTheme function when clicked
